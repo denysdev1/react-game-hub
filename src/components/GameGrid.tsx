767584104cdcba140c9bf8a42228e6a9fab7067f
Denys Kleimenov <denys.kleimenov.dev@gmail.com>
2023-09-04 17:38:01 +0300

    GameCardContainer, GameCard, GameCardSkeleton and PlatformIconList components were created

---
+++ src/components/GameGrid.tsx
@@ -0,0 +1,33 @@
+import { SimpleGrid, Text } from '@chakra-ui/react';
+import useGames from '../hooks/useGames';
+import { GameCard } from './GameCard';
+import { GameCardSkeleton } from './GameCardSkeleton';
+import { GameCardContainer } from './GameCardContainer';
+
+export const GameGrid = () => {
+  const { data, error, isLoading } = useGames();
+  const skeletons = [1, 2, 3, 4, 5, 6];
+
+  return (
+    <>
+      {error && <Text>{error}</Text>}
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        padding='10px'
+        spacing={3}
+      >
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <GameCardContainer key={skeleton}>
+              <GameCardSkeleton />
+            </GameCardContainer>
+          ))}
+        {data.map((game) => (
+          <GameCardContainer key={game.name}>
+            <GameCard game={game} />
+          </GameCardContainer>
+        ))}
+      </SimpleGrid>
+    </>
+  );
+};
