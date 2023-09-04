ded9d710f8eedfc0194bec49e4ae55fca0e15c51
Denys Kleimenov <denys.kleimenov.dev@gmail.com>
2023-09-04 17:24:01 +0300

    A generic data fetching hook and PlatformIconList component were created

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,4 @@
-import { useEffect, useState } from 'react';
-import apiClient from '../services/api-client';
-import { CanceledError } from 'axios';
+import useData from './useData';
 
 export type Platform = {
   id: number;
@@ -13,32 +11,9 @@ export type Game = {
   name: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
+  metacritic: number;
 };
 
-type FetchGamesResponse = {
-  count: number;
-  results: Game[];
-};
-
-const useGames = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState('');
-
-  useEffect(() => {
-    const controller = new AbortController();
-
-    apiClient
-      .get<FetchGamesResponse>('/games', { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-      });
-
-    return () => controller.abort();
-  }, []);
-
-  return { games, error };
-};
+const useGames = () => useData<Game>('/games');
 
 export default useGames;
---
+++ src/hooks/useGenres.ts
@@ -0,0 +1,10 @@
+import useData from './useData';
+
+export type Genre = {
+  id: number;
+  name: string;
+};
+
+const useGenres = () => useData<Genre>('/genres');
+
+export default useGenres;
