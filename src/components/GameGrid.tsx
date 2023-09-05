17d058ac95ed476b5ca3aa0ace53e9409cb5e10d
Denys Kleimenov <denys.kleimenov.dev@gmail.com>
2023-09-05 18:22:08 +0300

    Implemented infinite scroll

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,10 +1,11 @@
-import { Box, Button, SimpleGrid, Text } from '@chakra-ui/react';
+import { SimpleGrid, Text } from '@chakra-ui/react';
 import useGames from '../hooks/useGames';
 import { GameCard } from './GameCard';
 import { GameCardSkeleton } from './GameCardSkeleton';
 import { GameCardContainer } from './GameCardContainer';
 import { FC, Fragment } from 'react';
 import { GameQuery } from '../App';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 type Props = {
   gameQuery: GameQuery;
@@ -20,14 +21,25 @@ export const GameGrid: FC<Props> = ({ gameQuery }) => {
     hasNextPage,
   } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
+  const fetchedGamesCount =
+    data?.pages.reduce((acc, curr) => acc + curr.results.length, 0) || 0;
 
   if (error) {
     return <Text>{error.message}</Text>;
   }
 
   return (
-    <Box padding='10px'>
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={6}>
+    <InfiniteScroll
+      dataLength={fetchedGamesCount}
+      next={fetchNextPage}
+      hasMore={!!hasNextPage}
+      loader={<p>Loading...</p>}
+    >
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        spacing={6}
+        padding='10px'
+      >
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer key={skeleton}>
@@ -44,11 +56,6 @@ export const GameGrid: FC<Props> = ({ gameQuery }) => {
           </Fragment>
         ))}
       </SimpleGrid>
-      {hasNextPage && (
-        <Button onClick={() => fetchNextPage()} my={5}>
-          {isFetchingNextPage ? 'Loading...' : 'Load More'}
-        </Button>
-      )}
-    </Box>
+    </InfiniteScroll>
   );
 };
