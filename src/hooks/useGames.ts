38f3f6cca127f03dc4be2547112a565ec5ccbf66
Denys Kleimenov <denys.kleimenov.dev@gmail.com>
2023-09-05 17:51:11 +0300

    Refactor: create reusable API client

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,7 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import { GameQuery } from '../App';
 import { Platform } from './usePlatforms';
-import apiClient, { FetchResponse } from '../services/api-client';
+import APIClient, { FetchResponse } from '../services/api-client';
+
+const apiClient = new APIClient<Game>('/games');
 
 export type Game = {
   id: number;
@@ -16,16 +18,14 @@ const useGames = (gameQuery: GameQuery) => {
   return useQuery<FetchResponse<Game>, Error>({
     queryKey: ['games', gameQuery],
     queryFn: () =>
-      apiClient
-        .get<FetchResponse<Game>>('/games', {
-          params: {
-            genres: gameQuery.genre?.id,
-            parent_platforms: gameQuery.platform?.id,
-            ordering: gameQuery.sortOrder,
-            search: gameQuery.searchText,
-          },
-        })
-        .then((res) => res.data),
+      apiClient.getAll({
+        params: {
+          genres: gameQuery.genre?.id,
+          parent_platforms: gameQuery.platform?.id,
+          ordering: gameQuery.sortOrder,
+          search: gameQuery.searchText,
+        },
+      }),
   });
 };
 
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,13 +1,27 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 
 export type FetchResponse<T> = {
   count: number;
   results: T[];
 };
 
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
     key: import.meta.env.VITE_API_KEY,
   },
 });
+
+class APIClient<T> {
+  constructor(public endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config?: AxiosRequestConfig) => {
+    return axiosInstance  
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  }
+}
+
+export default APIClient;
