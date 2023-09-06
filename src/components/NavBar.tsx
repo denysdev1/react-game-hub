9c77ccbea3a8a7fa42591fe83c9231671cafdb18
Denys Kleimenov <denys.kleimenov.dev@gmail.com>
2023-09-06 16:00:14 +0300

    Fixed the NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,11 +3,14 @@ import Logo from '../assets/logo.webp';
 import { ColorModeSwitch } from './ColorModeSwitch';
 import { SearchInput } from './SearchInput';
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 export const NavBar: FC = () => {
   return (
     <HStack padding='10px'>
-      <Image src={Logo} boxSize='60px' />
+      <Link to='/'>
+        <Image src={Logo} boxSize='60px' objectFit='cover' />
+      </Link>
       <SearchInput />
       <ColorModeSwitch />
     </HStack>
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,15 +2,18 @@ import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
 import { FC, useRef } from 'react';
 import { BsSearch } from 'react-icons/bs';
 import useGameQueryStore from '../store/gameQueryStore';
+import { useNavigate } from 'react-router-dom';
 
 export const SearchInput: FC = () => {
   const setSearchText = useGameQueryStore((s) => s.setSearchText);
+  const navigate = useNavigate();
   const inputRef = useRef<HTMLInputElement>(null);
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     if (inputRef.current) {
+      navigate('/');
       setSearchText(inputRef.current.value);
     }
   };
